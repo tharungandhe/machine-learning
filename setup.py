5b0601174ae01d40d09ba5a11b3226dd1fdb4031
Tharun <tharungandhe@gmail.com>
2026-02-10 15:52:08 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-def read_requirements():
-    with open("requirements.txt") as f:
-        return f.read().splitlines()
+def get_requirements(file_path):
+    """
+    This function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+
+    return requirements
+
 
 setup(
-    name="ml_project",
-    version="0.1.0",
-    author="Your Name",
-    description="End-to-end Machine Learning project",
+    name="mlproject",
+    version="0.0.1",
+    author="Tharun",
+    author_email="tharun@email.com",
     packages=find_packages(),
-    install_requires=read_requirements(),
+    install_requires=get_requirements("requirements.txt"),
 )
